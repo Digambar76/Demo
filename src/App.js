bec69832f628280befe5b6c822f5944be94a5dc3
Digambar76 <146747528+Digambar76@users.noreply.github.com>
2025-09-23 17:08:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+//import {
+//  BrowserRouter as Router,
+// Routes,
+ // Route
+//} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    
+    }
+  }
+  return ( 
+  <>
+  {/* <Router> */}
+        <Navbar title="Demo1" about="About" toggleMode={toggleMode} mode={mode}/>
+        <div className="container">
+         {/* <Routes> */}
+            {/* <Route path="/about" element={<About mode={mode} />} /> */}
+           {/*<Route path="/" element={*/} 
+              <TextForm heading="Enter the text to analyze" mode={mode} />
+              {/* </Route> }
+          {/* </Routes> */}
+        </div>
+      {/* </Router> */}
+  </>
+ );
 }
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,49 @@
+
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpclick = ()=>{
+      let newText = text.toUpperCase();
+      setText(newText); 
+    }
+   const handleLoclick = ()=>{
+      let newText = text.toLowerCase();
+      setText(newText); 
+    }
+  
+
+    const handleOnChange = (event)=>{
+      setText(event.target.value);
+    }
+    const [text, setText] = useState('');
+  return (
+<>
+
+    <div className="container my-3" style={{color: props.mode==='dark'?'#042743':'white', }}>
+      <h1 style={{color: props.mode==='light'?'#042743':'white', }}>{props.heading} </h1>
+      <form>
+        <div className="mb-3">
+          <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'#042743'}} value={text} onChange={handleOnChange} id="textBox" rows="8"></textarea>
+        </div>
+        <button type='button' className="btn btn-primary mx-2" onClick={handleUpclick}>Convert to uppercase</button>
+        <button type='button' className="btn btn-primary mx-2" onClick={handleLoclick}>Convert to lowercase</button>
+
+      </form>
+    </div>
+    <div className="container my-3" style={{color: props.mode==='light'?'#042743':'white', }}>
+        <h2>Your text summary</h2> 
+        
+        <p>
+  {text.length} characters and {text.trim().length === 0 ? 0 : text.trim().split(/\s+/).length} words
+</p>
+
+        <h2>Preview</h2>
+        <p>{text}</p>
+   
+
+    </div>
+    </>
+
+  )
+}
+
